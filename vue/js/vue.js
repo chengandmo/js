a1f05e9c7c5adc3ae4f6d9db38bb0509c3260c32
cheng <18650738@qq.com>
2017-11-30 16:37:09 +0800

    todos.html

--- vue/js/vue.js
+++ vue/js/vue.js
@@ -6106,11 +6106,11 @@ Vue$3.prototype.$mount = function (
 
 if ("development" !== 'production' &&
     inBrowser && typeof console !== 'undefined') {
-  console[console.info ? 'info' : 'log'](
+  /*console[console.info ? 'info' : 'log'](
     "You are running Vue in development mode.\n" +
     "Make sure to turn on production mode when deploying for production.\n" +
     "See more tips at https://vuejs.org/guide/deployment.html"
-  );
+  );*/
 }
 
 // devtools global hook
@@ -6123,10 +6123,10 @@ setTimeout(function () {
       "development" !== 'production' &&
       inBrowser && !isEdge && /Chrome\/\d+/.test(window.navigator.userAgent)
     ) {
-      console[console.info ? 'info' : 'log'](
+     /* console[console.info ? 'info' : 'log'](
         'Download the Vue Devtools extension for a better development experience:\n' +
         'https://github.com/vuejs/vue-devtools'
-      );
+      );*/
     }
   }
 }, 0);
